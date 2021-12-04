e88d121cb850d880065527a9820a737d3aa3b109
Martin Petr <martin.petr.276@gmail.com>
2021-12-04 15:28:32 +0100

    First day - second part

--- 01/main.hs
+++ 01/main.hs
@@ -1,11 +1,24 @@
+-- https://adventofcode.com/2021/day/1
+
 readDepths :: [String] -> [Integer]
 readDepths = map read
 
 countIncreasingDepth :: [Integer] -> Integer
 countIncreasingDepth (x:y:xs) = (if x < y then 1 else 0) + countIncreasingDepth (y : xs)
-countIncreasingDepth xs = 0
+countIncreasingDepth _ = 0
 
-main :: IO ()
-main = do
+getMeasurementWindows :: [Integer] -> [Integer]
+getMeasurementWindows (x:y:z:xs) = x+y+z : getMeasurementWindows (y:z:xs)
+getMeasurementWindows _ = []
+
+-- First assignment
+ptOne :: IO ()
+ptOne = do
   input <- readFile "input.txt"
   print $ countIncreasingDepth $ readDepths $ lines input
+
+-- Second assignment
+ptTwo :: IO ()
+ptTwo = do
+  input <- readFile "input.txt"
+  print $ countIncreasingDepth $ getMeasurementWindows $ readDepths $ lines input
