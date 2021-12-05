103d0fad282697ce402c59b262c3eb80b99b8348
Martin Petr <martin.petr.276@gmail.com>
2021-12-05 15:24:15 +0100

    Fourth day - part two

--- 04/main.hs
+++ 04/main.hs
@@ -1,16 +1,13 @@
 -- https://adventofcode.com/2021/day/4
-import Data.Map (Map, fromList, update, elems, keys)
-import qualified Data.Map (filter)
-import Data.List.Split (splitOn)
 import Data.Char (isSpace)
+import Data.List (transpose, find)
+import Data.List.Split (splitOn)
 import Data.Maybe
-import Data.List (find)
 
-type Row = Map Int Bool
-type Board = [Row]
+type Board = [[(Maybe Int)]]
 
-constructRow :: String -> Row
-constructRow x = fromList $ (map (\y -> (y, False)) . ((map read) . words)) x
+constructRow :: String -> [(Maybe Int)]
+constructRow = map (\y -> (Just y)) . ((map read) . words)
 
 constructBoard :: [String] -> ([String], Board)
 constructBoard (y:ys) 
@@ -24,32 +21,53 @@ constructBoards [] = []
 constructBoards xs = nd : constructBoards st
   where (st, nd) = constructBoard xs 
 
-drawNumber :: [Int] -> (Int, [Int])
-drawNumber x = (head x, drop 1 x)
-
 updateBoard :: Int -> Board -> Board 
-updateBoard x = map (update (\_ ->(Just True)) x)
+updateBoard x = map (map (mark (Just x)))
+  where mark x y | x == y = Nothing
+                 | otherwise = y
+
+checkBoardWin :: Board -> Bool
+checkBoardWin x = check x || check (transpose x)
+  where
+    check = any (\y -> all isNothing y)
 
 checkWin :: [Board] -> Maybe Board
-checkWin = find checkBoardWin 
-  where checkBoardWin = any (\y -> all id (elems y))
+checkWin = find checkBoardWin
 
-calcScore :: Int -> Maybe Board -> Int
-calcScore x (Just ys) = sum (map (\y -> sum (keys (Data.Map.filter not y))) ys) * x
-calcScore _ _ = 0
+getNumber :: Maybe Int -> Int
+getNumber (Just x) = x
+getNumber Nothing = 0
+
+calcScore :: (Int, Maybe Board) -> Int
+calcScore (x, Just ys) = sum (map (\y -> sum (map getNumber y)) ys) * x
+calcScore _ = 0
 
 play :: [Int] -> [Board] -> Int
 play [] zs = 0
-play xs zs = if isNothing winner then play ys updated else calcScore y winner
+play (x:xs) zs = if isNothing winner then play xs updated else calcScore (x, winner)
   where 
-    (y,ys) = drawNumber xs
-    updated = (map (\z -> updateBoard y z) zs)
+    updated = (map (\z -> updateBoard x z) zs)
     winner = checkWin updated
+
+playWithLastWin :: [Int] -> [Board] -> [(Int, Maybe Board)] -> Int
+playWithLastWin _ [] winners = calcScore (last winners)
+playWithLastWin [] _ [] = 0
+playWithLastWin (x:xs) zs winners = if null xs 
+  then calcScore (last newWinners)
+  else playWithLastWin xs (filter (not . checkBoardWin) updated) newWinners
+  where 
+    updated = (map (\z -> updateBoard x z) zs)
+    newWinners = case (checkWin updated) of
+      Nothing -> winners
+      y -> winners ++ [(x, y)]
  
--- First assignment
-ptOne :: IO ()
-ptOne = do
+main :: IO ()
+main = do
   input <- readFile "input.txt"
   let (toBeDrawn:_:rest) = lines input
   let score = play (map read (splitOn "," toBeDrawn)) (constructBoards rest)
+  let score2 = playWithLastWin (map read (splitOn "," toBeDrawn)) (constructBoards rest) []
+  -- First assignment
   print $ score
+  -- Second assignment
+  print $ score2
